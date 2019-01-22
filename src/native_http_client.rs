69fee2adb744844a2de27cac90dc3603ef51b019
Sumeet Agarwal <sumeet.a@gmail.com>
2019-01-22 17:40:05 +0900

    scrapping it, i think i have an idea

--- src/native_http_client.rs
+++ src/native_http_client.rs
@@ -1,4 +1,5 @@
-use reqwest::{r#async::Client};
+//use reqwest::{r#async::Client};
+use yukikaze::client::{Client, Request as YRequest, HttpClient};
 use http::{Request,Response};
 use std::future::Future;
 use futures::stream::Stream;
@@ -8,22 +9,46 @@ use super::asynk::forward;
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
-pub async fn fetch(request: Request<String>) -> Result<Response<String>> {
-    let resp = await!(forward(Client::new()
-        .request(request.method().clone(), &request.uri().to_string())
-        .headers(request.headers().clone())
-        .body(request.body().clone())
-        .send()))?;
+//pub async fn fetch(request: Request<String>) -> Result<Response<String>> {
+//    let resp = await!(forward(Client::new()
+//        .request(request.method().clone(), &request.uri().to_string())
+//        .headers(request.headers().clone())
+//        .body(request.body().clone())
+//        .send()))?;
+//
+//    let request_url: String = request.uri().to_string();
+//    let status = resp.status();
+//    let body = await!(forward(resp.into_body().concat2()))?;
+//    let body = String::from_utf8_lossy(&body);
+//    Ok(Response::builder()
+//        .status(status)
+//        .body(body.into())
+//        .unwrap())
+//}
 
-    let request_url: String = request.uri().to_string();
+
+pub async fn fetch(request: Request<String>) -> Result<Response<String>> {
+    // keep the client in the environment maybe???? to take advantage of connection pooling!
+    // TODO: get rid of the unwraps
+    let client = Client::default();
+    println!("B");
+
+    let request = YRequest::new(
+        request.method().into(),
+        request.uri().to_string()
+    ).unwrap().body(Some(request.body().as_bytes()));
+
+    println!("C");
+
+    let resp = await!(forward(client.execute(request))).unwrap();
+
+    println!("D");
+
     let status = resp.status();
-    let body = await!(forward(resp.into_body().concat2()))?;
-    let body = String::from_utf8_lossy(&body);
+    let body = await!(forward(resp.text())).unwrap();
+
     Ok(Response::builder()
         .status(status)
-        .body(body.into())
-        .unwrap())
+        .body(body)?
+    )
 }
-
-
-
--- src/tokio_executor.rs
+++ src/tokio_executor.rs
@@ -1,27 +1,120 @@
-use std::future::Future as NewFuture;
+//use std::future::Future as NewFuture;
+//use tokio_current_thread::CurrentThread;
+////use tokio_current_thread::TaskExecutor;
+//use std::time;
+//use super::asynk::{backward};
+//
+//#[derive(Debug)]
+//pub struct AsyncExecutor {
+//    current_thread: CurrentThread,
+//}
+//
+//impl AsyncExecutor {
+//    pub fn new() -> Self {
+//        Self { current_thread: CurrentThread::new() }
+//    }
+//
+//    pub fn turn(&mut self) {
+//        let duration = time::Duration::from_millis(30);
+//        self.current_thread.turn(Some(duration)).unwrap();
+//    }
+//
+//    pub fn exec<I, E: std::fmt::Debug, F: NewFuture<Output = Result<I, E>> + 'static>(&mut self, future: F) {
+//        self.current_thread.spawn(backward(async {
+//            await!(future).unwrap();
+//            Ok(())
+//        }));
+//    }
+//}
+
+use std::io::Error as IoError;
+use std::time::{Duration, Instant};
+
+use futures::{future, Future};
 use tokio_current_thread::CurrentThread;
+use tokio_reactor::Reactor;
+use tokio_timer::timer::{self, Timer};
 use std::time;
 use super::asynk::{backward};
+use std::future::Future as NewFuture;
+use std::cell::RefCell;
 
 #[derive(Debug)]
 pub struct AsyncExecutor {
-    current_thread: CurrentThread,
+    reactor_handle: tokio_reactor::Handle,
+    timer_handle: tokio_timer::timer::Handle,
+    executor: RefCell<CurrentThread<tokio_timer::Timer<Reactor, tokio_timer::clock::Clock>>>,
+    enter: RefCell<tokio_executor::Enter>,
 }
 
 impl AsyncExecutor {
     pub fn new() -> Self {
-        Self { current_thread: CurrentThread::new() }
+        // We need a reactor to receive events about IO objects from kernel
+        let reactor = Reactor::new().unwrap();
+        let reactor_handle = reactor.handle();
+        // Place a timer wheel on top of the reactor. If there are no timeouts to fire, it'll let the
+        // reactor pick up some new external events.
+        let timer = Timer::new(reactor);
+        let timer_handle = timer.handle();
+        // And now put a single-threaded executor on top of the timer. When there are no futures ready
+        // to do something, it'll let the timer or the reactor generate some new stimuli for the
+        // futures to continue in their life.
+        let executor = CurrentThread::new_with_park(timer);
+        let enter = tokio_executor::enter().expect("Multiple executors at once");
+
+        Self { reactor_handle, timer_handle, executor: RefCell::new(executor), enter: RefCell::new(enter) }
     }
 
     pub fn turn(&mut self) {
-        let duration = time::Duration::from_millis(10);
-        self.current_thread.turn(Some(duration)).unwrap();
+        // This will set the default handle and timer to use inside the closure and run the future.
+        tokio_reactor::with_default(&self.reactor_handle, &mut self.enter.borrow_mut(), |enter| {
+            timer::with_default(&self.timer_handle, enter, |enter| {
+                // The TaskExecutor is a fake executor that looks into the current single-threaded
+                // executor when used. This is a trick, because we need two mutable references to the
+                // executor (one to run the provided future, another to install as the default one). We
+                // use the fake one here as the default one.
+                let mut default_executor = tokio_current_thread::TaskExecutor::current();
+                tokio_executor::with_default(&mut default_executor, enter, |enter| {
+                    let duration = time::Duration::from_millis(10);
+                    self.executor.borrow_mut().turn(Some(duration)).unwrap();
+                });
+            });
+        });
     }
 
     pub fn exec<I, E: std::fmt::Debug, F: NewFuture<Output = Result<I, E>> + 'static>(&mut self, future: F) {
-        self.current_thread.spawn(backward(async {
-            await!(future).unwrap();
-            Ok(())
-        }));
+        // This will set the default handle and timer to use inside the closure and run the future.
+        tokio_reactor::with_default(&self.reactor_handle, &mut self.enter.borrow_mut(), |enter| {
+            timer::with_default(&self.timer_handle, enter, |enter| {
+                // The TaskExecutor is a fake executor that looks into the current single-threaded
+                // executor when used. This is a trick, because we need two mutable references to the
+                // executor (one to run the provided future, another to install as the default one). We
+                // use the fake one here as the default one.
+                let mut default_executor = tokio_current_thread::TaskExecutor::current();
+                tokio_executor::with_default(&mut default_executor, enter, |enter| {
+                    self.executor.borrow_mut().spawn(backward(async {
+                        await!(future).unwrap();
+                        Ok(())
+                    }));
+                });
+            });
+        });
     }
+
+//    fn with_context<F: FnOnce(&mut Self)>(&mut self, mut run_under_context: F) {
+//        let mut enter = tokio_executor::enter().expect("Multiple executors at once");
+//        // This will set the default handle and timer to use inside the closure and run the future.
+//        tokio_reactor::with_default(&self.reactor_handle, &mut enter, |enter| {
+//            timer::with_default(&self.timer_handle, enter, |enter| {
+//                // The TaskExecutor is a fake executor that looks into the current single-threaded
+//                // executor when used. This is a trick, because we need two mutable references to the
+//                // executor (one to run the provided future, another to install as the default one). We
+//                // use the fake one here as the default one.
+//                let mut default_executor = tokio_current_thread::TaskExecutor::current();
+//                tokio_executor::with_default(&mut default_executor, enter, |enter| {
+//                    run_under_context(&mut self)
+//                });
+//            });
+//        });
+//    }
 }
