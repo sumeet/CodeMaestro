f19971cae38a734cd9420202a196c36cbbb449ba
Sumeet Agarwal <sumeet.a@gmail.com>
2019-04-05 17:59:23 -0700

    ok now try this

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -3,7 +3,13 @@ set -e
 
 # grab exports from the rust buildpack. see
 # https://github.com/emk/heroku-buildpack-rust/blob/master/bin/compile#L79-L95
-BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
-. $BP_DIR/export
+
+# Our rustup installation.
+export RUSTUP_HOME="$CACHE_DIR/multirust"
+# Our cargo installation.  We implicitly trust Rustup and Cargo
+# to do the right thing when new versions are released.
+export CARGO_HOME="$CACHE_DIR/cargo"
+# Include binaries installed by cargo and rustup in our path.
+export PATH="\$CARGO_HOME/bin:\$PATH"
 
 ./scripts/run-on-server-release
