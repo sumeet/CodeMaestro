22976373c1f23cbef084d8bd50640de5c4ab99bc
Sumeet Agarwal <sumeet.a@gmail.com>
2019-04-05 19:14:25 -0700

    add caching for wasm builds on dokku

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -12,4 +12,7 @@ export CARGO_HOME="$CACHE_DIR/cargo"
 # Include binaries installed by cargo and rustup in our path.
 export PATH="$CARGO_HOME/bin:$PATH"
 
+# need this or else we have to rebuild all of WASM every single time
+export CARGO_TARGET_DIR="$CACHE_DIR/target"
+
 ./scripts/run-on-server-release
