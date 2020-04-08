c2dc33e37f702aee0d318472d536d6bdfc19856e
Sumeet Agarwal <sumeet.a@gmail.com>
2020-04-07 21:32:35 -0700

    wip

--- editor/src/editor/value_renderer.rs
+++ editor/src/editor/value_renderer.rs
@@ -1,8 +1,11 @@
+// TODO: many of the draw functions in here are copy and pasted from CodeEditorRenderer...
+// ... i couldn't find a good way of sharing the code, but i think i might need to eventually.
+// for now it's just copy+paste
 use crate::align;
 use crate::colorscheme;
-use crate::ui_toolkit::{DrawFnRef, UiToolkit};
+use crate::ui_toolkit::{Color, UiToolkit};
 use cs::env::ExecutionEnvironment;
-use cs::lang::{StructValues, Value};
+use cs::lang::Value;
 use cs::{lang, EnvGenie};
 use lazy_static::lazy_static;
 
@@ -39,12 +42,9 @@ impl<'a, T: UiToolkit> ValueRenderer<'a, T> {
                     FALSE_LABEL.clone()
                 }
             }
-            Value::String(string) => format!("{}{}{}",
-                                             lang::STRING_TYPESPEC.symbol,
-                                             string,
-                                             lang::STRING_TYPESPEC.symbol),
+            Value::String(string) => return self.render_string(string),
             Value::Error(e) => format!("Error: {:?}", e),
-            Value::Number(num) => num.to_string(),
+            Value::Number(num) => return self.render_number(num),
             Value::List(_) => {
                 panic!("let's worry about lists later, they're not even in the example")
             }
@@ -66,14 +66,25 @@ impl<'a, T: UiToolkit> ValueRenderer<'a, T> {
                 panic!("let's worry about lists later, they're not even in the example")
             }
         };
-        self.draw_buttony_text(&label)
+        self.draw_buttony_text_hardcoded_color(&label)
+    }
+
+    fn render_number(&self, value: &i128) -> T::DrawResult {
+        self.ui_toolkit
+            .draw_buttony_text(&value.to_string(), colorscheme!(literal_bg_color))
+    }
+
+    fn render_string(&self, value: &str) -> T::DrawResult {
+        self.draw_buttony_text(&format!("\u{F10D} {} \u{F10E}", value),
+                               colorscheme!(literal_bg_color))
     }
 
     fn render_struct_symbol_and_name_button(&self, struct_id: &lang::ID) -> T::DrawResult {
         let typ = lang::Type::from_spec_id(*struct_id, vec![]);
         let type_display_info = self.env_genie.get_type_display_info(&typ).unwrap();
-        self.draw_buttony_text(&format!("{:?} {:?}",
-                                        type_display_info.symbol, type_display_info.name))
+        self.draw_buttony_text_hardcoded_color(&format!("{} {}",
+                                                        type_display_info.symbol,
+                                                        type_display_info.name))
     }
 
     fn render_struct_field_value(&self,
@@ -89,13 +100,16 @@ impl<'a, T: UiToolkit> ValueRenderer<'a, T> {
                                     .unwrap();
         let arg_name = &strukt_field.name;
         self.ui_toolkit.draw_all_on_same_line(&[
-            &|| self.draw_buttony_text(&format!("{:?} {:?}", type_display_info.symbol, arg_name)),
+            &|| self.draw_buttony_text_hardcoded_color(&format!("{} {}", type_display_info.symbol, arg_name)),
             &|| Self::new(self.env, value, self.ui_toolkit).render(),
         ])
     }
 
-    fn draw_buttony_text(&self, label: &str) -> T::DrawResult {
-        self.ui_toolkit
-            .draw_buttony_text(label, colorscheme!(menubar_color))
+    fn draw_buttony_text(&self, label: &str, color: Color) -> T::DrawResult {
+        self.ui_toolkit.draw_buttony_text(label, color)
+    }
+
+    fn draw_buttony_text_hardcoded_color(&self, label: &str) -> T::DrawResult {
+        self.draw_buttony_text(label, colorscheme!(menubar_color))
     }
 }
