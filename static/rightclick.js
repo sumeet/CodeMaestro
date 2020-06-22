bfa9bdcab7876e3351c5fcb46ff6d0542af2101b
Sumeet Agarwal <sumeet.a@gmail.com>
2020-06-21 18:57:04 -0700

    wasm: shift the right click menu down to match normal right click menu behavior

--- static/rightclick.js
+++ static/rightclick.js
@@ -1,6 +1,12 @@
 function showRightClickMenu(contextMenuEl, contextMenuTriggerEl, drawContextMenuActiveOverlay, pageX, pageY) {
     // haxx, have to account for this because of body { zoom: } in main.css
     const globalZoom = 110;
+
+    // shift everything down to the right a bit, because that's how real right click menus work. they open close,
+    // but not exactly where you right click, and you have to move the mouse to select the first menu item
+    pageX += 5;
+    pageY += 5;
+
     if (contextMenuEl) {
         contextMenuEl.style.left = `${pageX * (100 / globalZoom)}px`;
         contextMenuEl.style.top = `${pageY * (100 / globalZoom)}px`;
